b38e6f564ffc89d887a08d1780e28aa1e881a552
Dev Patel <devpatel9773@gmail.com>
2025-03-16 13:14:33 +0530

    Fix landing page to direct login

--- client/src/components/common/CheckAuth.jsx
+++ client/src/components/common/CheckAuth.jsx
@@ -4,11 +4,16 @@ import { Navigate , useLocation } from 'react-router-dom'
 function CheckAuth({isAuthenticated,user,children}) {
   const location = useLocation()
 
+  if (!isAuthenticated && location.pathname === "/") {
+   return <Navigate to="/auth/login" />;
+ }
+ 
   if(!isAuthenticated && !( location.pathname.includes('/auth/login') || location.pathname.includes('/auth/register'))){
      return(
         <Navigate to='/auth/login'></Navigate>
      )
   } 
+
 
   if(isAuthenticated && ( location.pathname.includes('/auth/login') || location.pathname.includes('/auth/register'))){
      if(user?.role === 'admin'){
